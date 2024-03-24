aaa6b08a9207c327a033ed4429baa97a99db07ba
Ali <a.djatdoev@ideas-world.com>
2024-03-24 21:42:43 +0400

    refactor

--- src/main/java/com/example/springapp1/client/fallback/UserClientFallback.java
+++ src/main/java/com/example/springapp1/client/fallback/UserClientFallback.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
